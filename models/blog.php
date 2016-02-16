ed7b13eb8c5bc544a1bb9aef9538bd11409e5b26
amico <amico8amico@gmail.com>
2016-02-16 15:34:52 +0800

    データベース接続

--- models/blog.php
+++ models/blog.php
@@ -1,7 +1,25 @@
 <?php
 	class Blog {
+		// プロパティ
+		private $dbconnect = '';
+
+		// コンストラクタ
+		public function __construct() {
+			// DB接続ファイルの読込
+			require('dbconnect.php');
+			// DB接続設定の値を代入
+			$this->dbconnect = $db;
+		}
+
 		public function index() {
-			echo 'モデルのindex()メソッドが呼ばれました！';
+			$sql = 'SELECT * FROM `blogs` WHERE `delete_flag` = 0';
+			$results = mysqli_query($this->dbconnect, $sql) or die(mysqli_error($this->dbconnect));
+
+			while ($result = mysqli_fetch_assoc($results)) {
+				echo $result['id'].'<br>';
+				echo $result['title'].'<br>';
+				echo $result['created'].'<br>';
+			}
 		}
 	}
 ?>
